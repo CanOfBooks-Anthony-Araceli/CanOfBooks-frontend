0126ee9558b2276653efa573e014964d1e9e0c5f
Araceligm90 <garcia.araceli90@gmail.com>
2023-02-15 23:29:13 -0500

    added delete button and delete function

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -4,6 +4,7 @@ import Carousel from 'react-bootstrap/Carousel'
 import Button from 'react-bootstrap/Button';
 import BookFormModal from './BookFormModal';
 
+
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
@@ -49,6 +50,22 @@ class BestBooks extends React.Component {
     }
   }
 
+  deleteBook = async (_id) => {
+  try{
+    let url = `${process.env.REACT_APP_SERVER}/books/${_id}`;
+    await axios.delete(url);
+    let updatedBooks = this.state.books.filter(book => book._id !== _id);
+    this.setState({
+      books: updatedBooks
+    })
+  }
+  catch (error) {
+    console.error(error)
+  }
+  }
+
+
+
   showModal = () => {
     this.setState({
      showModal: true
@@ -72,14 +89,15 @@ class BestBooks extends React.Component {
         {this.state.books.length ? (
           <Carousel fade>
             {this.state.books.map(book => 
-             <Carousel.Item key={book._id}>
-              <img 
-              src='https://via.placeholder.com/1500x400.jpeg?text=Book+Image'
-              alt={book.title}
-              />
+              <Carousel.Item key={book._id}>
+            <img 
+            src='https://via.placeholder.com/1500x400.jpeg?text=Book+Image'
+            alt={book.title}
+            />
               <Carousel.Caption>
                 <h3>Title: {book.title}</h3>
                 <p>Description: {book.desc}</p>
+                <Button variant='danger' onClick={() => this.deleteBook(book._id)} type='button'>Delete book</Button>
               </Carousel.Caption>
              </Carousel.Item>
              )}
